9637d1b8a53490dd98e672c31579575df558c1bb
Dawid <dawid.ney@gmail.com>
2023-10-21 18:30:40 +0200

    Changed indent, removed obsolete code

--- KptnVerifyInvitationCode.cs
+++ KptnVerifyInvitationCode.cs
@@ -16,99 +16,93 @@ using System.Linq;
 
 namespace YourNamespace
 {
-    public class UserVerificationEntity  : ITableEntity
-    {
-        public string PartitionKey { get; set; }
-        public string RowKey { get; set; }
-        public DateTimeOffset? Timestamp { get; set; }
-        public ETag ETag { get; set; }
-        public string EMAIL { get; set; }
-        public string INVITATION_CODE { get; set; }
-    }
+  public class UserVerificationEntity : ITableEntity
+  {
+    public string PartitionKey { get; set; }
+    public string RowKey { get; set; }
+    public DateTimeOffset? Timestamp { get; set; }
+    public ETag ETag { get; set; }
+    public string EMAIL { get; set; }
+    public string INVITATION_CODE { get; set; }
+  }
 
-    public static class KptnVerifyInvitationCode
+  public static class KptnVerifyInvitationCode
+  {
+    [FunctionName("KptnVerifyInvitationCode")]
+    public static async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        ILogger log)
     {
-        [FunctionName("KptnVerifyInvitationCode")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
+      log.LogInformation("C# HTTP trigger function processed a request.");
+
+      string authHeader = req.Headers["Authorization"];
+      if (string.IsNullOrEmpty(authHeader))
+      {
+        log.LogWarning("Invalid Authentication");
+        return new UnauthorizedResult();
+      }
+
+      var authParts = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader.Split(' ')[1])).Split(':');
+      var username = authParts[0];
+      var password = authParts[1];
+
+      if (username != Environment.GetEnvironmentVariable("BASIC_AUTH_USERNAME") ||
+          password != Environment.GetEnvironmentVariable("BASIC_AUTH_PASSWORD"))
+      {
+        log.LogWarning("Invalid Authentication");
+        return new UnauthorizedResult();
+      }
+
+      var invitationCodeAttributeKey = $"extension_{Environment.GetEnvironmentVariable("B2C_EXTENSIONS_APP_ID").Replace("-", "")}_InvitationCode";
+      var requestBody = new System.IO.StreamReader(req.Body).ReadToEnd();
+      var jsonData = JObject.Parse(requestBody);
+      var invitationCode = jsonData[invitationCodeAttributeKey]?.ToString();
+      var email = jsonData["email"]?.ToString();
+
+      if (string.IsNullOrEmpty(invitationCode))
+      {
+        return new BadRequestObjectResult(new
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+          version = "1.0.0",
+          status = 400,
+          action = "ValidationError",
+          userMessage = "Please provide an invitation code."
+        });
+      }
 
-            string authHeader = req.Headers["Authorization"];
-            if (string.IsNullOrEmpty(authHeader))
-            {
-                log.LogWarning("Invalid Authentication");
-                return new UnauthorizedResult();
-            }
+      // get invitation code from azure storage table
+      string tableName = "UserVerificationCode";
+      string storageAccountConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
 
-            var authParts = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader.Split(' ')[1])).Split(':');
-            var username = authParts[0];
-            var password = authParts[1];
+      // Create a TableClient
+      TableClient tableClient = new(storageAccountConnectionString, tableName);
+      // Define the query
+      Expression<Func<UserVerificationEntity, bool>> filterExpression = entity =>
+          entity.EMAIL == email &&
+          entity.INVITATION_CODE == invitationCode;
 
-            if (username != Environment.GetEnvironmentVariable("BASIC_AUTH_USERNAME") ||
-                password != Environment.GetEnvironmentVariable("BASIC_AUTH_PASSWORD"))
-            {
-                log.LogWarning("Invalid Authentication");
-                return new UnauthorizedResult();
-            }
+      string filter = TableClient.CreateQueryFilter(filterExpression);
+      
+      // Execute the query
+      var entity = await tableClient.QueryAsync<UserVerificationEntity>(filter).FirstOrDefaultAsync();
 
-            var invitationCodeAttributeKey = $"extension_{Environment.GetEnvironmentVariable("B2C_EXTENSIONS_APP_ID").Replace("-", "")}_InvitationCode";
-            var requestBody = new System.IO.StreamReader(req.Body).ReadToEnd();
-            var jsonData = JObject.Parse(requestBody);
-            var invitationCode = jsonData[invitationCodeAttributeKey]?.ToString();
-            var email = jsonData["email"]?.ToString();
+      if (entity == null || entity.INVITATION_CODE != invitationCode || entity.EMAIL != email)
+      {
+        return new BadRequestObjectResult(new
+        {
+          version = "1.0.0",
+          status = 400,
+          action = "ValidationError",
+          userMessage = "Please provide correct e-mail and invitation code."
+        });
+      }
 
-            if (string.IsNullOrEmpty(invitationCode))
-            {
-                return new BadRequestObjectResult(new
-                {
-                    version = "1.0.0",
-                    status = 400,
-                    action = "ValidationError",
-                    userMessage = "Please provide an invitation code."
-                });
-            }
-
-            // get invitation code from azure storage table
-            string tableName = "UserVerificationCode";
-            string storageAccountConnectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
-
-            // Create a TableClient
-            TableClient tableClient = new(storageAccountConnectionString, tableName);
-            // Define the query
-            Expression<Func<UserVerificationEntity, bool>> filterExpression = entity =>
-                entity.EMAIL == email &&
-                entity.INVITATION_CODE == invitationCode;
-
-            string filter = TableClient.CreateQueryFilter(filterExpression);
-            
-            // Query the table
-            await foreach (var en in tableClient.QueryAsync<TableEntity>())
-            {
-                Console.WriteLine(en);
-            }
-
-            // Execute the query
-            var entity = await tableClient.QueryAsync<UserVerificationEntity>(filter).FirstOrDefaultAsync();
-            
-            if (entity == null || entity.INVITATION_CODE != invitationCode || entity.EMAIL != email)
-            {
-                return new BadRequestObjectResult(new
-                {
-                    version = "1.0.0",
-                    status = 400,
-                    action = "ValidationError",
-                    userMessage = "Please provide correct e-mail and invitation code."
-                });
-            }
-
-            return new OkObjectResult(new Dictionary<string, object>
-            {
-                ["version"] = "1.0.0",
-                ["action"] = "Continue",
-                [invitationCodeAttributeKey] = ""
-            });
-        }
+      return new OkObjectResult(new Dictionary<string, object>
+      {
+        ["version"] = "1.0.0",
+        ["action"] = "Continue",
+        [invitationCodeAttributeKey] = ""
+      });
     }
+  }
 }
